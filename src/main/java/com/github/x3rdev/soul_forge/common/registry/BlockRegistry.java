5ce09b009a2524fea6b517355f48603cc606c8eb
x3rdev <nicolaeungur@gmail.com>
2024-07-11 05:28:42 -0700

    soul brick door

--- src/main/java/com/github/x3rdev/soul_forge/common/registry/BlockRegistry.java
+++ src/main/java/com/github/x3rdev/soul_forge/common/registry/BlockRegistry.java
@@ -3,6 +3,7 @@ package com.github.x3rdev.soul_forge.common.registry;
 import com.github.x3rdev.soul_forge.SoulForge;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.properties.BlockSetType;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -24,6 +25,6 @@ public class BlockRegistry {
             () -> new Block(BlockBehaviour.Properties.copy(Blocks.CHISELED_STONE_BRICKS)));
     public static final RegistryObject<Block> SOUL_BRICK_COLUMN = BLOCKS.register("soul_brick_column",
             () -> new RotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.STONE_BRICKS)));
-
-
+    public static final RegistryObject<Block> SOUL_BRICK_DOOR = BLOCKS.register("soul_brick_door",
+            () -> new DoorBlock(BlockBehaviour.Properties.copy(Blocks.IRON_DOOR), BlockSetType.POLISHED_BLACKSTONE));
 }
