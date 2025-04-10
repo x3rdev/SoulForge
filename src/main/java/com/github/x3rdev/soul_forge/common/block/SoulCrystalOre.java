1d34467577bfeeff8ba16296b5acfab5c6ab9535
Reavez <roma.generaloff@gmail.com>
2025-04-10 16:32:32 +0300

    soul crystal ore block fix #2

--- src/main/java/com/github/x3rdev/soul_forge/common/block/SoulCrystalOre.java
+++ src/main/java/com/github/x3rdev/soul_forge/common/block/SoulCrystalOre.java
@@ -4,6 +4,6 @@ import net.minecraft.world.level.block.Block;
 
 public class SoulCrystalOre extends Block {
     public SoulCrystalOre(Properties pProperties) {
-        super(pProperties.lightLevel(value -> 1));
+        super(pProperties.lightLevel(value -> 1).strength(3.0f));
     }
 }
--- src/main/java/com/github/x3rdev/soul_forge/common/registry/BlockRegistry.java
+++ src/main/java/com/github/x3rdev/soul_forge/common/registry/BlockRegistry.java
@@ -43,5 +43,5 @@ public class BlockRegistry {
     public static final DeferredHolder<Block, Block> SOUL_CHANDELIER = BLOCKS.register("soul_chandelier",
             () -> new SoulChandelierBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.LANTERN)));
     public static final DeferredHolder<Block, Block> SOUL_CRYSTAL_ORE = BLOCKS.register("soul_crystal_ore",
-            () -> new SoulCrystalOre(BlockBehaviour.Properties.ofFullCopy(Blocks.DIAMOND_ORE)));
-    }
+            () -> new SoulCrystalOre(BlockBehaviour.Properties.of()));
+}
