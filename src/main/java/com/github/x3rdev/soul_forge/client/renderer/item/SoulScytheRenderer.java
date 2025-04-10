68ec2d4da4e0948f081629045d3e6b23927fd925
x3rdev <nicolaeungur@gmail.com>
2025-04-09 20:25:48 -0700

    changed soul scythe glowing effect to be stronger
    Took 5 minutes

--- src/main/java/com/github/x3rdev/soul_forge/client/renderer/item/SoulScytheRenderer.java
+++ src/main/java/com/github/x3rdev/soul_forge/client/renderer/item/SoulScytheRenderer.java
@@ -7,8 +7,10 @@ import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.resources.ResourceLocation;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib.cache.object.BakedGeoModel;
+import software.bernie.geckolib.cache.texture.AutoGlowingTexture;
 import software.bernie.geckolib.model.DefaultedItemGeoModel;
 import software.bernie.geckolib.model.GeoModel;
 import software.bernie.geckolib.renderer.GeoItemRenderer;
@@ -17,7 +19,12 @@ import software.bernie.geckolib.renderer.layer.AutoGlowingGeoLayer;
 public class SoulScytheRenderer extends GeoItemRenderer<SoulScytheItem> {
     public SoulScytheRenderer() {
         super(new DefaultedItemGeoModel<>(ResourceLocation.fromNamespaceAndPath(SoulForge.MOD_ID, "soul_scythe")));
-        addRenderLayer(new AutoGlowingGeoLayer<>(this));
+        addRenderLayer(new AutoGlowingGeoLayer<>(this) {
+            @Override
+            protected @NotNull RenderType getRenderType(SoulScytheItem animatable, @Nullable MultiBufferSource bufferSource) {
+                return RenderType.eyes(AutoGlowingTexture.getEmissiveResource(getTextureResource(animatable)));
+            }
+        });
     }
 
 }
