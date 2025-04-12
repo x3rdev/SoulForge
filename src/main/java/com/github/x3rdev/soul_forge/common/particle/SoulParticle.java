3f2b6ac62e13fa7b9637b9c653af65591cbf20c0
Reavez <roma.generaloff@gmail.com>
2025-04-12 03:05:30 +0300

    class naming fix

--- src/main/java/com/github/x3rdev/soul_forge/common/particle/SoulParticle.java
+++ src/main/java/com/github/x3rdev/soul_forge/common/particle/SoulParticle.java
@@ -1,14 +1,13 @@
 package com.github.x3rdev.soul_forge.common.particle;
 
-import com.github.x3rdev.soul_forge.common.registry.ParticleRegistry;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.particle.*;
 import net.minecraft.core.particles.SimpleParticleType;
 import org.jetbrains.annotations.Nullable;
 
-public class SoulPacticle extends TextureSheetParticle {
+public class SoulParticle extends TextureSheetParticle {
         private final SpriteSet spriteSet;
-        protected SoulPacticle (ClientLevel level, double x,double y, double z, SpriteSet spriteSet, double xSpeed, double ySpeed, double zSpeed) {
+        protected SoulParticle (ClientLevel level, double x,double y, double z, SpriteSet spriteSet, double xSpeed, double ySpeed, double zSpeed) {
             super(level, x, y, x, xSpeed, ySpeed, zSpeed);
             this.spriteSet = spriteSet;
             this.gravity = 0;
@@ -28,7 +27,7 @@ public static class Provider implements ParticleProvider<SimpleParticleType>{
 
     @Override
     public @Nullable Particle createParticle(SimpleParticleType type, ClientLevel level, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed) {
-        return new SoulPacticle(level, x, y, z, this.spriteSet, xSpeed, ySpeed, zSpeed);
+        return new SoulParticle(level, x, y, z, this.spriteSet, xSpeed, ySpeed, zSpeed);
     }
 }
 }
