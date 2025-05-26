a2f0c861e5b5aa695f65b500726fafbed8211141
x3rdev <nicolaeungur@gmail.com>
2025-05-26 16:27:04 -0700

    more pedestal block work
    Took 29 minutes

--- src/main/java/com/github/x3rdev/soul_forge/common/block_entity/PedestalBlockEntity.java
+++ src/main/java/com/github/x3rdev/soul_forge/common/block_entity/PedestalBlockEntity.java
@@ -2,18 +2,23 @@ package com.github.x3rdev.soul_forge.common.block_entity;
 
 import com.github.x3rdev.soul_forge.common.registry.BlockEntityRegistry;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.NonNullList;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.Container;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
-import software.bernie.geckolib.GeckoLib;
 import software.bernie.geckolib.animatable.GeoBlockEntity;
 import software.bernie.geckolib.animatable.instance.AnimatableInstanceCache;
 import software.bernie.geckolib.animation.AnimatableManager;
 import software.bernie.geckolib.util.GeckoLibUtil;
 
-public class PedestalBlockEntity extends BlockEntity implements GeoBlockEntity {
+public class PedestalBlockEntity extends BlockEntity implements GeoBlockEntity, Container {
 
+    private final NonNullList<ItemStack> items = NonNullList.withSize(getContainerSize(), ItemStack.EMPTY);
     private final AnimatableInstanceCache cache = GeckoLibUtil.createInstanceCache(this);
 
     public PedestalBlockEntity(BlockPos pos, BlockState blockState) {
@@ -27,6 +32,59 @@ public class PedestalBlockEntity extends BlockEntity implements GeoBlockEntity {
     private void spawnErrorParticles() {
 
     }
+
+    @Override
+    protected void loadAdditional(CompoundTag tag, HolderLookup.Provider registries) {
+        super.loadAdditional(tag, registries);
+    }
+
+    @Override
+    protected void saveAdditional(CompoundTag tag, HolderLookup.Provider registries) {
+        super.saveAdditional(tag, registries);
+    }
+
+    @Override
+    public int getContainerSize() {
+        return 1;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return items.stream().allMatch(ItemStack::isEmpty);
+    }
+
+    @Override
+    public ItemStack getItem(int slot) {
+        return items.get(slot);
+    }
+
+    @Override
+    public ItemStack removeItem(int slot, int amount) {
+        items.get(slot)
+        return null;
+    }
+
+    @Override
+    public ItemStack removeItemNoUpdate(int slot) {
+        return null;
+    }
+
+    @Override
+    public void setItem(int slot, ItemStack stack) {
+
+    }
+
+    @Override
+    public boolean stillValid(Player player) {
+        return false;
+    }
+
+    @Override
+    public void clearContent() {
+
+    }
+
+    // client code start
 
     @Override
     public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
