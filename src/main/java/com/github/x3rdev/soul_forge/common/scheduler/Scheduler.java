26312c45daa2e14308680ff305579459f0b2d55e
x3rdev <nicolaeungur@gmail.com>
2025-05-28 14:50:56 -0700

    added scheduler
    Took 1 hour 30 minutes

---
+++ src/main/java/com/github/x3rdev/soul_forge/common/scheduler/Scheduler.java
@@ -0,0 +1,44 @@
+package com.github.x3rdev.soul_forge.common.scheduler;
+
+
+import com.github.x3rdev.soul_forge.SoulForge;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.tick.ServerTickEvent;
+import net.neoforged.neoforge.server.ServerLifecycleHooks;
+
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+@EventBusSubscriber(bus = EventBusSubscriber.Bus.GAME, modid = SoulForge.MOD_ID)
+public final class Scheduler {
+
+    static final ConcurrentMap<Integer, List<Runnable>> SERVER_SCHEDULE = new ConcurrentHashMap<>();
+
+    private Scheduler() {
+
+    }
+
+    public static void schedule(Runnable task, int delay) {
+        SERVER_SCHEDULE.compute(ServerLifecycleHooks.getCurrentServer().getTickCount() + delay,
+                (integer, runnables) -> {
+                    if(runnables == null) {
+                        runnables = new ObjectArrayList<>();
+                    }
+                    runnables.add(task);
+                    return runnables;
+                });
+    }
+    @SubscribeEvent
+    public static void serverTick(ServerTickEvent event) {
+        int ticks = ServerLifecycleHooks.getCurrentServer().getTickCount();
+        List<Runnable> tasks = SERVER_SCHEDULE.get(ticks);
+        if(tasks != null) {
+            tasks.forEach(Runnable::run);
+            SERVER_SCHEDULE.remove(ticks);
+        }
+    }
+}
