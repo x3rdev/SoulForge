5dea44107d41c88c24a1e9719175d7b1e9b6b7b0
Nicolae U. <78513008+x3rdev@users.noreply.github.com>
2025-11-28 00:53:19 -0800

    added shader compatibility for `soul` shader

--- src/main/java/com/github/x3rdev/soul_forge/common/compat/IrisCompat.java
+++ src/main/java/com/github/x3rdev/soul_forge/common/compat/IrisCompat.java
@@ -1,10 +1,12 @@
 package com.github.x3rdev.soul_forge.common.compat;
 
+import net.irisshaders.iris.api.v0.IrisApi;
+
 public class IrisCompat {
 
     public static boolean areShadersLoaded() {
         if(ModCompatibility.irisModPresent()) {
-
+            return IrisApi.getInstance().isShaderPackInUse();
         }
         return false;
     }
